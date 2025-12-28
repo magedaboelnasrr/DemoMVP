435c8349efd3753ede1f9a14913934da821c1583
Maged Abo Elnasr <maged.aboelnasr@gmail.com>
2025-12-28 15:08:55 +0200

    Add project files.

---
+++ DemoMVP/Controllers/ProductsController.cs
@@ -0,0 +1,60 @@
+﻿using DemoMVP.Context;
+using DemoMVP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVP.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Products.ToList());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+                return View(product);
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            _context.Products.Update(product);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        
+        public IActionResult Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+                return NotFound();
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
